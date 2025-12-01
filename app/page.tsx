63541eb42e4dfdb5d3100595cdd21434fe19848f
KKhushbu <khushbukushwah27@gmail.com>
2025-12-01 12:51:27 +0530

    "add Thank you"

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hey Khushbu, now you can start.tsx file.
+            Hey Khushbu, now you can start.tsx file thank you.
           </h1>
 
           <button
